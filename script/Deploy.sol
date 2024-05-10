43d71fd7ba915a2d2574162ce8ab9fdac4a77a39
m-r-g-t <133902786+m-r-g-t@users.noreply.github.com>
2024-05-10 19:04:31 +0200

    feat: inherit rfpsimplestrategy

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -1,33 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-import {Greeter} from 'contracts/Greeter.sol';
 import {Script} from 'forge-std/Script.sol';
 import {IERC20} from 'forge-std/interfaces/IERC20.sol';
+import {Metadata} from 'contracts/utils/Metadata.sol';
+import {IRegistry} from 'interfaces/utils/IRegistry.sol';
 
-contract Deploy is Script {
-  struct DeploymentParams {
-    string greeting;
-    IERC20 token;
+contract CreateProfile is Script {
+  struct ProfileParams {
+    uint256 _nonce;
+    string _name;
+    Metadata _metadata;
+    address _owner;
+    address[] _members;
+  }
+
+  struct AlloParams {
+    address _alloRegistry;
   }
 
   /// @notice Deployment parameters for each chain
-  mapping(uint256 _chainId => DeploymentParams _params) internal _deploymentParams;
+  mapping(uint256 _chainId => ProfileParams _params) internal _profileParams;
+  mapping(uint256 _chainId => AlloParams _params) internal _alloParams;
 
   function setUp() public {
-    // Mainnet
-    _deploymentParams[1] = DeploymentParams('Hello, Mainnet!', IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2));
+    // Optimism Sepolia
+    Metadata memory _metadata = Metadata(1, 'ipfs://ipfs_address');
+    address[] memory _members = new address[](1);
+    _members[0] = msg.sender;
+    _profileParams[11_155_111] = ProfileParams(4, 'PORCO DIO', _metadata, msg.sender, _members);
 
-    // Sepolia
-    _deploymentParams[11_155_111] =
-      DeploymentParams('Hello, Sepolia!', IERC20(0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6));
+    _alloParams[11_155_111] = AlloParams(0x4AAcca72145e1dF2aeC137E1f3C5E3D75DB8b5f3);
   }
 
   function run() public {
-    DeploymentParams memory _params = _deploymentParams[block.chainid];
+    ProfileParams memory _params = _profileParams[block.chainid];
+    AlloParams memory _allo = _alloParams[block.chainid];
 
+    // create a profile on allo
+    IRegistry _registry = IRegistry(_allo._alloRegistry);
     vm.startBroadcast();
-    new Greeter(_params.greeting, _params.token);
+    _registry.createProfile(_params._nonce, _params._name, _params._metadata, _params._owner, _params._members);
     vm.stopBroadcast();
   }
 }
